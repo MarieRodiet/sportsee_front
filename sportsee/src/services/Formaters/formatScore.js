44a2c92b0ac58525c86fd931ff3e9968a8c5221e
Marie Rodiet <marie.rodiet@gmail.com>
2022-06-13 09:29:24 +0200

    Format Score Data

--- sportsee/src/services/Formaters/formatScore.js
+++ sportsee/src/services/Formaters/formatScore.js
@@ -1,16 +1,6 @@
 import propTypes from 'prop-types';
 
 export default function formatScore(data) {
-    console.log()
-    //let newScore = -1
-    // if (data.hasOwnProperty('todayScore')) {
-    //     console.log("data.hasOwnProperty todayScore")
-    //     newScore = data.todayScore * 100;
-    // }
-    // else {
-    //     console.log("DOES NOT HAVE data.hasOwnProperty todayScore")
-    //     newScore = data.score * 100;
-    // }
     return data.hasOwnProperty('todayScore') ?
         { ...data, todayScore: data.todayScore * 100 } : {
             ...data, todayScore: data.score * 100
