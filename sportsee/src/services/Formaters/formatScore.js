ce9b66910702eddb933d52e232e8fa20440a216b
Marie Rodiet <marie.rodiet@gmail.com>
2022-06-13 09:29:03 +0200

    Refactor Formating Score data

--- sportsee/src/services/Formaters/formatScore.js
+++ sportsee/src/services/Formaters/formatScore.js
@@ -1,15 +1,20 @@
 import propTypes from 'prop-types';
 
 export default function formatScore(data) {
-
-    let newScore = -1
-    if (data.hasOwnProperty('todayScore')) {
-        newScore = data.todayScore * 100;
-    }
-    else {
-        newScore = data.score * 100;
-    }
-    return { ...data, score: newScore };
+    console.log()
+    //let newScore = -1
+    // if (data.hasOwnProperty('todayScore')) {
+    //     console.log("data.hasOwnProperty todayScore")
+    //     newScore = data.todayScore * 100;
+    // }
+    // else {
+    //     console.log("DOES NOT HAVE data.hasOwnProperty todayScore")
+    //     newScore = data.score * 100;
+    // }
+    return data.hasOwnProperty('todayScore') ?
+        { ...data, todayScore: data.todayScore * 100 } : {
+            ...data, todayScore: data.score * 100
+        };
 }
 
 formatScore.propTypes = {
