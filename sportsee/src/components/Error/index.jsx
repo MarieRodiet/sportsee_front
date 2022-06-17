390801fc3e98e3fae50244b2ddde32738823bfaf
Marie Rodiet <marie.rodiet@gmail.com>
2022-06-17 12:42:32 +0200

    Remove Links from Error Page

--- sportsee/src/components/Error/index.jsx
+++ sportsee/src/components/Error/index.jsx
@@ -1,5 +1,4 @@
 import './../../styles/_error.scss'
-import { Link } from 'react-router-dom'
 /**
  *
  * @returns jsx for displaying Error when fetching data has failed
@@ -9,13 +8,6 @@ export default function Error() {
         <div className="App-Error">
             <h1 className="App-Error-code">404</h1>
             <h2 className="App-Error-text">there is an error!&nbsp;&nbsp;🫤</h2>
-
-            <Link className="App-Error-advice" to="/user/12">
-                Click to see Karl's results
-            </Link>
-            <Link className="App-Error-advice" to="/user/18">
-                Click to see Cecilia's results
-            </Link>
         </div>
     )
 }
